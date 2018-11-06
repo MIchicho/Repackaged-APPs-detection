09c8747231fea40d71dc053dee20380828bc9feb
Chicho <chichoxian@gmail.com>
2018-11-06 11:18:18 +0800

    Delete identify_native.sh

--- ResDroid/identify_native.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-INPUT=./app_3rdmarkets.csv
-
-while read LINE
-do
-    apktool_output=$(echo $LINE | awk -F',' '{print $2}')
-    [ -e $apktool_output/lib ] && find $apktool_output/lib -type f | xargs file
-done < $INPUT
